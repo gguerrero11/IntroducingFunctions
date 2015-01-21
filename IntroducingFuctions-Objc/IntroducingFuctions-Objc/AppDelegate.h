382692881c371cbc9751ff4f969e5d1279b37d23
gguerrero11 <gguerrero11@gmail.com>
2015-01-21 00:56:44 -0800

    added documentation

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.h
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.h
@@ -12,8 +12,22 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+// Declare a function calle printIntroductions
+// The function should accept an NSString as a parameter called name
+
+  /*!
+   * Prints out my name with an introduction.
+   * Parameter expects NSString name.
+   */
 - (void)printIntroductions:(NSString *)name;
 
+
+// Add a new function called countDownTillIntroduction: that accepts
+// an integer called numberofDays as an argument
+  /*!
+   * Outputs a countdown of the amount of days.
+   * Parameter expects int numberOfDays.
+   */
 - (void)countDownTillIntroduction:(int)numberOfDays;
 
 
--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -16,37 +16,64 @@
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Declare and set a variable called myName to your name
     NSString *myName = @"Gabe";
+    // before you call printIntroductions call countDownTillIntroduction and pass in 4
     [self countDownTillIntroduction:4];
+    
+    // call printIntroductions: and pass in 'myName' as an argument
     [self printIntroductions:myName];
+    
+    /*
+     You should see it print
+     "4 days left until introductions",
+     "3 days left until introductions", 
+     "2 days left until introductions",
+     "1 days left until introductions",
+     and then it should print "The time has come" and
+     "My name is Gabe
+     Gabe to moushimasu"
+     
+     */
+    
+    
     return YES;
 }
 
 - (void)printIntroductions:(NSString *)name {
     
+    // Declare two more string variables introduction and japaneseIntroduction both with an NSString type
     NSString *introduction;
     NSString *japaneseIntroduction;
     
+    //Set japanese introduction using stringByAppendingString instance method by appending " to moushimasu"
     introduction = [NSString stringWithFormat:@"My name is %@", name];
     japaneseIntroduction = [name stringByAppendingString:@" to moushimasu"];
     
+    //Print introduction and japaneseIntroduction
     NSLog(@"%@ \n%@", introduction, japaneseIntroduction);
     
 }
 
+
+// Step 4: Recurse through a function
+
 - (void)countDownTillIntroduction:(int)numberOfDays {
-    
+    // Check to see if the numberOfDays is 0
     if(numberOfDays == 0) {
+        // If the number of days is 0, it should print "The time has come"
         NSLog(@"The time has come");
     } else {
+        // If numberOfDays is not 0
+        // It should print "[numberOfDays] days left until introductions"
         NSLog(@"%d days left until introductions", numberOfDays);
+        // Declare and set a variable oneLessDay and set it to the numberOfDays - 1
         int oneLessDay = numberOfDays - 1;
+        // Call countDownTillIntroduction and pass in oneLessDay
         [self countDownTillIntroduction:oneLessDay];
     
     }
 }
-
-// Define new functions here...
 
 
 
